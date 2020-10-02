c525d0c5ce191c752fe5d17627692dde13285670
eloback <eduardolloback@gmail.com>
2020-10-02 18:39:19 -0300

    Organização das pastas, e novo deck editor
    - alterada a organização das pastas para facilitar o trabalho
    - função de buscar carta corrigida

--- Card/CardBase.gd
+++ Card/CardBase.gd
@@ -8,8 +8,6 @@ export var cardName = "example"
 signal mouse_pressed
 
 var MouseOver = false
-export var size_loocked = true
-var cardSize = rect_size
 
 func _input(event):
 	if event is InputEventMouseButton:
@@ -22,33 +20,16 @@ func _ready():
 	var texturePath = cardDatabase.get_texture_path(cardName)
 
 	if texturePath != null:
-
 		$Card.texture = load(texturePath)
-		if size_loocked:
-			$Card.scale = cardSize/$Card.texture.get_size()
-			$CardBack.scale = cardSize/$CardBack.texture.get_size()
-		else:
-			$Card.scale = rect_size/$Card.texture.get_size()
-			$CardBack.scale = rect_size/$CardBack.texture.get_size()
+		$CardBack.texture = load("res://Assets/Textures/CardBack.jpg")
+		$Card.scale = rect_size/$Card.texture.get_size()
+		$CardBack.scale = rect_size/$CardBack.texture.get_size()
 		$Card.show()
 		pass # Replace with function body.
 
-func _change_card_in_display(card):
-	cardName = card.cardName
-	$Card.texture = load(cardDatabase.get_texture_path(cardName))
-	if size_loocked:
-		$Card.scale = cardSize/$Card.texture.get_size()
-	else:
-		$Card.scale = rect_size/$Card.texture.get_size()
-
-
-func _inicializa_card_back(): #carrega imagem no card_back ao ser chamado
-	$CardBack.texture = load("res://Assets/Textures/CardBack.jpg")
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
+func _change_card_in_display(card, cardBase):
+	$Card.texture = load(cardBase.get_texture_path(card))
+	$Card.scale = rect_size/$Card.texture.get_size()
 
 func _on_CardBase_mouse_entered():
 	MouseOver = true
