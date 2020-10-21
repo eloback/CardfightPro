36955b857951afa566ced1bf8b9f19d1bbfa2e70
eloback <eduardolloback@gmail.com>
2020-10-21 08:07:00 -0300

    Correções finais no file system
    Adicionado alguns comentarios, correção de bugs e etc...

--- Scenes/DeckEditor/NewDeckEditor.gd
+++ Scenes/DeckEditor/NewDeckEditor.gd
@@ -1,31 +1,16 @@
 extends Control
 var cardDB = preload("res://Card/CardDatabase.gd").new()
 
+func _ready(): #carrega o ultimo deck que o usuário estava modificando
+	$DeckPreview._load_deck()
+
 func _get_card_db():
 	return cardDB
 
-func _set_deck(newDeck):
-	if _check_deck(newDeck):
-		$DeckPreview.deck = newDeck
-		$DeckPreview._load_deck()
-	else:
-		push_warning("Deck Invalido")
+func _set_deck(newDeck): #carrega o deck escolhido pelo usuario, no deckPreview
+	$DeckPreview.deck = newDeck
+	$DeckPreview._load_deck()
 	
-func _check_deck(deck):
-	if !deck:
-		return false
-	var count = 0
-	for card in deck:
-		if !cardDB.cards.has(card):
-			return false
-		var cardNumber = deck.get(card)
-		if cardNumber < 1 or cardNumber > 4:
-			return false
-		count += cardNumber
-	if count > 50:
-		return false
-	print(count)
-	return true
 #add and remove card são chamadas quando o usuario aperta em alguma carta dos nodes SearchedCardContainer ou DeckPreview
 func _add_card_to_deck(card):
 	$DeckPreview._add_card_to_deck(card)
@@ -33,9 +18,6 @@ func _add_card_to_deck(card):
 
 func _remove_card_from_deck(card):
 	$DeckPreview._remove_card_from_deck(card)
-	$DeckPreview._load_deck()
-
-func _ready(): #carrega o ultimo deck que o usuário estava modificando
 	$DeckPreview._load_deck()
 
 func load_deck():
@@ -47,7 +29,6 @@ func save_deck():
 func _on_LoadDeckButton_pressed(): #precisa de parametro de deck_name
 	load_deck()
 	pass
-
 
 func _on_SaveDeckButton_pressed(): #precisa de parametro de deck_name
 	$FileController.deck = $DeckPreview.deck
--- Scenes/File_Selector/File_Selector.gd
+++ Scenes/File_Selector/File_Selector.gd
@@ -1,14 +1,33 @@
+#instruções sobre funcionamento da cena
+#	Carrega o deck escolhido pelo usuario e salva seu conteudo no 
+#	dicionario "deck", ao mesmo tempo emite o sinal "deckLoaded.
+#	Também pode salvar um deck no diretorio escolhindo pelo usuario
 extends Control
 var deck = {}
+var cardDB = preload("res://Card/CardDatabase.gd").new()
+signal deck_loaded(newDeck)
 
-func _ready():
-	$FileDialog.add_filter("*.dk ; Deck Files")
+func _ready(): # configurações do fileDialog
+	$FileDialog.add_filter("*.dk ; Deck Files") #file type suportado
 	$FileDialog.current_dir = "user://"
 	$FileDialog.current_file = "deck"
-	pass
 
-func _open_file_dialog(mode):
-	
+func _check_deck(deck): #verifica se o deck carregado é valido ou não
+	if !deck:
+		return false
+	var count = 0
+	for card in deck:
+		if !cardDB.cards.has(card):
+			return false
+		var cardNumber = deck.get(card)
+		if cardNumber < 1 or cardNumber > 4:
+			return false
+		count += cardNumber
+	if count > 50:
+		return false
+	return true
+
+func _open_file_dialog(mode): #abre e configura o dialog
 	match mode:
 		"open":
 			$FileDialog.disconnect("file_selected", self, "_save_file")
@@ -21,17 +40,15 @@ func _open_file_dialog(mode):
 	$FileDialog.popup()
 
 func _open_file(path):
-	print(path)
 	var f = File.new()
 	f.open(path, File.READ)
 	deck = f.get_var()
 	f.close()
-	$".."._set_deck(deck)
-	return
-
+	if _check_deck(deck):
+		emit_signal("deck_loaded", deck) #emite um sinal com o deck como parametro
+	push_warning("Deck Invalido")
 
 func _save_file(path):
-	print(path)
 	var save_deck = File.new()
 	save_deck.open(path, File.WRITE)
 	save_deck.store_var(deck)
